d825774ee067abfcf7d3e442be089851a6b9372a
atyukachev <atyukachev@bivgroup.ru>
2025-01-27 16:58:35 +0300

    [upd]: Использование библиотеки "Angular Material"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatFormField} from '@angular/material/form-field';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  standalone: true,
+  providers: [],
+  imports: [MatInputModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+}
