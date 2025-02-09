387917ee035c5bf6b3867d8a2a6e0fa7975cb893
tunka8090 <tunka8090@yandex.ru>
2025-02-09 23:34:52 +0300

    [add]: Настройка структуры проекта

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {MatFormField, MatSuffix} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-product-page',
-  imports: [RouterLink, MatFormField, MatInput, MatSuffix, MatDatepicker, MatDatepickerToggle, MatDatepickerInput],
+  imports: [RouterLink],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
