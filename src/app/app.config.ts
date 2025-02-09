d4f63eedefe1b94828debb8df941ee5f6f536f8c
tunka8090 <tunka8090@yandex.ru>
2025-02-09 23:53:41 +0300

    [add]: Настройка стилей проекта

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
     importProvidersFrom(BrowserAnimationsModule),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync(),
   ],
 };
